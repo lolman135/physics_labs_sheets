ef2ccd7667a28667a4ce58b719624ba71025ec82
lolman135 <148134717+lolman135@users.noreply.github.com>
2024-02-09 00:42:57 +0200

    Update functional.py

--- lab_1-1_table_generator/functional.py
+++ lab_1-1_table_generator/functional.py
@@ -6,11 +6,25 @@ def cell_name_forming(letter, index):
 
 def element_generating(a, d):
     element = uniform(a-d, a+d)
-    element = round(element, 4)
+    element = round(element, 2)
     return element
 
 def clean_sheet(sheet):
     for row in sheet.iter_rows(min_row=2):
         for cell in row:
             cell.value = None
-            
+
+def choice(a, b, i):
+    if i == 1: return a
+    else: return b
+
+def distribution(a, some):
+    h = -0.1
+    a = round(a, 4)
+    for key in some:
+        if a >= h and a < h + 0.01:
+            some[key].append(a)
+        h += 0.01      
+
+def inscription(x):
+    return str(round(x, 2)) + " <= ∆Ti < " + str(round(x+0.01, 2))
