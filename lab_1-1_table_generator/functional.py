016af69b95fd23be40b418b5af7ac922791e3684
lolman135 <148134717+lolman135@users.noreply.github.com>
2024-02-08 17:24:15 +0200

    Update functional.py

--- lab_1-1_table_generator/functional.py
+++ lab_1-1_table_generator/functional.py
@@ -13,7 +13,4 @@ def clean_sheet(sheet):
     for row in sheet.iter_rows(min_row=2):
         for cell in row:
             cell.value = None
-
-def one_oscillation(x):
-    return x/5
-
+            
